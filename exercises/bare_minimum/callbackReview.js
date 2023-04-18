0443bc761023cf6d5fb23c451737b8d22731f4d3
Andrew Lund <andrew.s.lund@gmail.com>
2023-04-18 09:35:38 -0700

    Complete bare_minimum/promiseConstructor.js and bare_minimum/callbackReview.js

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -8,18 +8,26 @@ var request = require('needle');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
-  fs.readFile(filePath, (err, data) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       // do some logic
+      callback(err);
     } else {
-      callback(null, data.split('/n').shift());
+      callback(null, data.split('\n').shift());
     }
   });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  request.get(url, (err, response) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, response.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
