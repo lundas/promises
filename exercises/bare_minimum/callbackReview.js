b90484e83efcc60ef9bd1df1bebfc1baf7dc13a3
Andrew Lund <andrew.s.lund@gmail.com>
2023-04-17 18:31:33 -0700

    started on callback review

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,8 +6,15 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      // do some logic
+    } else {
+      callback(null, data.split('/n').shift());
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
